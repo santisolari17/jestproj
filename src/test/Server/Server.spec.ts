034c5e875c1f3e63fc07aa59edb0923b5d8fed85
Santiago Pirela <s17manutd@gmail.com>
2022-11-08 01:53:04 -0300

    advanced testing use cases done

--- src/test/Server/Server.spec.ts
+++ src/test/Server/Server.spec.ts
@@ -1,27 +1,54 @@
+import { Authorizer } from '../../app/Authorization/Authorizer';
+import { UsersDBAccess } from '../../app/Data/UsersDBAccess';
+import { DataHandler } from '../../app/Handlers/DataHandler';
+import { LoginHandler } from '../../app/Handlers/LoginHandler';
 import { Server } from '../../app/Server/Server';
 
+jest.mock('../../app/Handlers/LoginHandler');
+jest.mock('../../app/Handlers/DataHandler');
+jest.mock('../../app/Authorization/Authorizer');
+
 const requestMock = {
-    url: ''
+	url: '',
 };
 const responseMock = {
-    end: jest.fn()
+	end: jest.fn(),
 };
 const listenMock = {
-    listen: jest.fn()
+	listen: jest.fn(),
 };
 
 
 jest.mock('http', () => ({
-    createServer: (cb: any) => {
-        cb(requestMock, responseMock)
-        return listenMock;
-    }
+	createServer: (cb: any) => {
+		cb(requestMock, responseMock)
+		return listenMock;
+	}
 }))
 
 describe.only('Server test suite', () => {
-    test('should create server on port 8080', () => {
-        new Server().startServer();
-        expect(listenMock.listen).toBeCalledWith(8080);
-        expect(responseMock.end).toBeCalled();
-    });
+	test('should create server on port 8080', () => {
+		new Server().startServer();
+		expect(listenMock.listen).toBeCalledWith(8080);
+		expect(responseMock.end).toBeCalled();
+	});
+	
+	test('should handle login requests', () => {
+		requestMock.url = 'http://localhost:8080/login';
+		new Server().startServer();
+
+		const handleRequestSpy = jest.spyOn(LoginHandler.prototype, 'handleRequest');
+
+		expect(handleRequestSpy).toBeCalled();
+		expect(LoginHandler).toBeCalledWith(requestMock, responseMock, expect.any(Authorizer));
+	});
+
+	test('should handle data requests', () => {
+		requestMock.url = 'http://localhost:8080/users';
+		new Server().startServer();
+		const handleRequestSpy = jest.spyOn(DataHandler.prototype, 'handleRequest');
+
+		expect(DataHandler).toBeCalledWith(requestMock, responseMock, expect.any(Authorizer), expect.any(UsersDBAccess));
+		expect(handleRequestSpy).toBeCalled();
+	})
 }); 
